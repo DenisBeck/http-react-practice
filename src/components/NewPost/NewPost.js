2cb5c06123ea747cd4230695230b9aad1d2707cd
DenisBeck <beckass86@gmail.com>
2018-10-31 16:04:40 +0500

    fetched post request

--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
 
 import './NewPost.css';
 
@@ -7,6 +8,19 @@ class NewPost extends Component {
         title: '',
         content: '',
         author: 'Denis'
+    }
+
+    postDataHandler = () => {
+        const post = {
+            title: this.state.title,
+            body: this.state.content,
+            author: this.state.author
+        }
+        Axios.post(
+            'https://jsonplaceholder.typicode.com/posts', post)
+                .then(response => {
+                    console.log(response); 
+                });
     }
 
     render () {
@@ -23,7 +37,7 @@ class NewPost extends Component {
                     <option value="Max">Max</option>
                     <option value="Manu">Manu</option>
                 </select>
-                <button>Add Post</button>
+                <button onClick={this.postDataHandler}>Add Post</button>
             </div>
         );
     }
